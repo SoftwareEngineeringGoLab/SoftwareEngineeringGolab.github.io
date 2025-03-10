a758a9bbaa0ccb1c8fbb958d7d90c34e251dda61
erfansadraiye <sadraiyeh.e2002@gmail.com>
2025-03-10 15:52:50 +0330

    Add Tips Component

---
+++ travel-destination-guide/src/components/Tips.vue
@@ -0,0 +1,17 @@
+<!--Simple tips for traveling!-->
+
+<template>
+  <section id="tips">
+    <h2>Travel Tips</h2>
+  </section>
+</template>
+
+<style scoped>
+  ul {
+    padding-left: 1.5rem;
+  }
+
+  ul li {
+    list-style-type: disc;
+  }
+</style>
