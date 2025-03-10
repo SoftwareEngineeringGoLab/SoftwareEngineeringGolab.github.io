64019285055c2dc251c225c966fff63acd9faaaa
erfansadraiye <sadraiyeh.e2002@gmail.com>
2025-03-10 15:53:32 +0330

    Add general travel tips dataset

--- travel-destination-guide/src/components/Tips.vue
+++ travel-destination-guide/src/components/Tips.vue
@@ -3,6 +3,12 @@
 <template>
   <section id="tips">
     <h2>Travel Tips</h2>
+    <ul>
+      <li>Bring a reusable water bottle.</li>
+      <li>Wear comfortable shoes.</li>
+      <li>Don't forget your passport!</li>
+      <li>Always carry cash.</li>
+    </ul>
   </section>
 </template>
 
